d15460597cc7be5244d5fd751a779f013d95ca05
jerome <jeromeparent9@gmail.com>
2020-06-18 16:08:27 -0400

    [ADD] changed index pictures into icons and card background color

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const redirect_uri = "https://spotify-track-finder.herokuapp.com/callback";
+const redirect_uri = "http://localhost:8888/callback";
 // http://localhost:8888/callback
 // https://spotify-track-finder.herokuapp.com/callback
 
