3a9883dc407f8a0a1ca10a4421b517b312a6f5a5
Jerome <jeromeparent9@gmail.com>
2020-08-04 12:26:19 -0400

    [ADD] styling home page, made feature sliders bigger on mobile

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.set("view engine", "ejs");
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const redirect_uri = "http://www.analyse-aud.io/callback";
-
+// http://www.analyse-aud.io/callback
+// http://localhost:8888/callback
 app.listen(port, function () {
   console.log(`Server Started: ${port}`);
 });
