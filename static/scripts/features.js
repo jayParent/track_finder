37bdefabfe8269faf5931e283827fa9b43359d9d
Jerome <jeromeparent9@gmail.com>
2020-08-12 13:18:48 -0400

    [FIX] recommendations, calculate track score [ADD] easy mode

--- static/scripts/features.js
+++ static/scripts/features.js
@@ -1,5 +1,4 @@
 const seeDetailsBtn = document.getElementById("features_see_details_btn");
-const easyModeBtn = document.getElementById("features_easy_mode_btn");
 
 seeDetailsBtn.addEventListener("click", () => {
     let view = document.getElementById("see_details_view");
